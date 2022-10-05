f1e9156ddf76df78a65846ba943e7d560c4369bf
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-05 11:50:21 +0300

    Wrote binToX function bodies of nbc class

--- src/cpp-files/nbc.cpp
+++ src/cpp-files/nbc.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "../header-files/nbc.h"
+
+std::string nbc::binToOct( const std::basic_string<char> &number ) {
+	return number.length() == 0 ? "0.0" : nbc::decToOct(binToDec(number));
+}
+
+std::string nbc::binToDec( std::basic_string<char> number ) {
+	if (number.length() == 0 || number == "0.0")
+		return "0.0";
+
+	std::string result;
+
+	if (number.at(0) == '-') {
+		number = number.substr(1, number.length());
+		result += "-";
+	}
+
+	// split the decimal number as it supposed to be
+	long int intPoint = 0;
+	if (number.length() != 0) {
+		intPoint = std::stoi(number.substr(0, number.find('.')));
+	}
+	long int decPoint = 0;
+	if (number.find('.') != -1 && number.length() > number.find('.') + 1) {
+		decPoint = std::stoi(
+				number.substr(number.find('.') + 1, number.length()));
+	}
+
+	// calculate the decimal number from binary
+	long int integer = 0;
+	for (int i = 0; intPoint > 0; i++) {
+		integer += intPoint % 10 * pow(2, i);
+		intPoint /= 10;
+	}
+
+	// add calculated integer value of decimal to result string
+	result += std::to_string(integer);
+	// add a '.' to the end
+	result.push_back('.');
+	// if there is an entered decimal point or not
+	if (decPoint != 0) {
+		// calculate the decimal point of binary to decimal
+		double decimal = 0;
+		for (int i = -1 * number.substr(number.find('.') + 1, number.length()).length(); decPoint > 0; i++) {
+			decimal += decPoint % 10 * pow(2, i);
+			decPoint /= 10;
+		}
+
+		// check the decimal point and add to end of string the char of number if decimal not fully 0
+		while (true) {
+			decimal *= 10;
+			if (fmodl(decimal, 1.0) == 0) {
+				result.push_back(static_cast<char>(decimal) + '0');
+				break;
+			} else {
+				result.push_back(static_cast<char>((( int ) decimal) + '0'));
+				decimal -= ( int ) decimal;
+			}
+		}
+	} else {
+		// if there isn't entered a decimal point enter .0
+		result.push_back('0');
+	}
+
+	return result;
+}
+
+std::string nbc::binToHex( const std::basic_string<char> &number ) {
+	return number.length() == 0 ? "0.0" : nbc::decToHex(binToDec(number));
+}
