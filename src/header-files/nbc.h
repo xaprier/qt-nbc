d8a70bcc9a2c6824ba2dc2c1a5a5fb85fecd2abc
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-08 17:32:57 +0300

    Added to header file
    Wrote octToX function prototypes of nbc class.

--- src/header-files/nbc.h
+++ src/header-files/nbc.h
@@ -1,18 +1,17 @@
 //
 // Created by xaprier on 8/23/22.
 //
-#include <string>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
 #include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 #ifndef QT_NBC_NBC_H
 #define QT_NBC_NBC_H
 
 class nbc {
   private:
-
   public:
 	static std::string binToOct( const std::basic_string<char> &number );
 
@@ -20,6 +19,11 @@ class nbc {
 
 	static std::string binToHex( const std::basic_string<char> &number );
 
+	static std::string octToBin( const std::basic_string<char> &number );
+
+	static std::string octToDec( std::basic_string<char> number );
+
+	static std::string octToHex( const std::basic_string<char> &number );
 };
 
-#endif //QT_NBC_NBC_H
+#endif  // QT_NBC_NBC_H
