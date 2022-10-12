ed4d0ebc6a081611e7cb9dcf3912fc3275598084
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-12 20:11:59 +0300

    Update nbc.h
    Added all the prototype functions of nbc class

--- src/header-files/nbc.h
+++ src/header-files/nbc.h
@@ -11,20 +11,30 @@
 #define QT_NBC_NBC_H
 
 class nbc {
-  private:
-  public:
-	static std::string binToOct( const std::basic_string<char> &number );
+   private:
+   public:
+    static std::string binToOct( const std::string &number );
 
-	static std::string binToDec( std::basic_string<char> number );
+    static std::string binToDec( std::basic_string<char> number );
 
-	static std::string binToHex( const std::basic_string<char> &number );
+    static std::string binToHex( const std::string &number );
 
-	static std::string octToBin( const std::basic_string<char> &number );
+    static std::string decToBin( std::basic_string<char> number );
 
-	static std::string octToDec( std::basic_string<char> number );
+    static std::string decToOct( std::basic_string<char> number );
 
+<<<<<<< Updated upstream
 	static std::string octToHex( const std::basic_string<char> &number );
 
+=======
+    static std::string decToHex( std::basic_string<char> number );
+
+    static std::string octToBin( const std::string &number );
+
+    static std::string octToDec( std::basic_string<char> number );
+
+    static std::string octToHex( const std::basic_string<char> &number );
+>>>>>>> Stashed changes
 };
 
 #endif  // QT_NBC_NBC_H
