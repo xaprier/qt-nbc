973dd3972225d92ac7d93e6d7ce807baf154152b
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-12 20:18:29 +0300

    New Forms
    Added new form for choosing the operation from ui(choose).
    
    This ui will create the other ui classes as dialogs.
    
    main.cpp calls the choose class for creating QMainWindow form

---
+++ src/header-files/choose.h
@@ -0,0 +1,32 @@
+//
+// Created by "xaprier" on "10/8/22"
+//
+
+#ifndef QT_NBC_CHOOSE_H
+#define QT_NBC_CHOOSE_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+	class choose;
+}
+QT_END_NAMESPACE
+
+class choose : public QMainWindow {
+  Q_OBJECT
+
+  public:
+	explicit choose( QWidget *parent = nullptr );
+
+	~choose() override;
+
+  private slots:
+
+	void onPushButton_clicked();
+
+  private:
+	Ui::choose *ui;
+};
+
+#endif  // QT_NBC_CHOOSE_H
