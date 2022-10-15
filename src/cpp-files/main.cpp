111fca05237896811a6f52d311bf8d08651ec466
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-15 05:11:42 +0300

    Removed comment lines

--- src/cpp-files/main.cpp
+++ src/cpp-files/main.cpp
@@ -1,18 +1,12 @@
 #include <QApplication>
 
 #include "../header-files/choose.h"
-#include "../header-files/nbconverter.h"
 #include "../header-files/nbcalculator.h"
+#include "../header-files/nbconverter.h"
 
 int main( int argc, char *argv[] ) {
-	QApplication a(argc, argv);
-	/*
-NBCalculator n;
-	n.show();
-	NBConverter n;
-	n.show();
-	 */
-	choose c;
-	c.show();
-	return QApplication::exec();
+    QApplication a( argc, argv );
+    choose c;
+    c.show();
+    return QApplication::exec();
 }
