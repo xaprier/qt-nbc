929539196ad27ed2bc26e904b1a4d33fc4208cf3
Seymen Kalkan <seymenkalkan@gmail.com>
2022-10-15 05:11:20 +0300

    Added inheritance and encapsulation to the classes

--- src/header-files/nbconverter.h
+++ src/header-files/nbconverter.h
@@ -1,34 +1,34 @@
 #include <QDialog>
 #include <QValidator>
 
+#include "nbc.h"
+
 #ifndef QT_NBC_NBCONVERTER_H
 #define QT_NBC_NBCONVERTER_H
 QT_BEGIN_NAMESPACE
 namespace Ui {
-	class NBConverter;
+    class NBConverter;
 }
 QT_END_NAMESPACE
 
+class NBConverter : public QDialog, public nbc {
+    Q_OBJECT
 
-class NBConverter : public QDialog {
-  Q_OBJECT
+   public:
+    explicit NBConverter( QWidget *parent = nullptr );
 
-  public:
+    ~NBConverter() override;
 
-	explicit NBConverter( QWidget *parent = nullptr );
+   private slots:
 
-	~NBConverter() override;
+    void comboChanged();
 
-  private slots:
+    void baseChanged();
 
-	void comboChanged();
+   private:
+    Ui::NBConverter *ui;
 
-	void baseChanged();
-
-  private:
-	Ui::NBConverter *ui;
-
-	QValidator *validator;
+    QValidator *validator;
 };
 
 #endif  // QT_NBC_NBCONVERTER_H
