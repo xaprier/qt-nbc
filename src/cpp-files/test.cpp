73b9907a807ff13ff0c61a7d218055e26553468f
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-19 15:19:48 +0300

    Added CTest

---
+++ src/cpp-files/test.cpp
@@ -0,0 +1,15 @@
+#include "../header-files/test_cases.h"
+
+int main() {
+    testConstructors();
+    testConverters();
+    ostreamOverloadTest();
+    operator_plusOverloading();
+    operator_subtractOverloading();
+    operator_multiplyOverloading();
+    operator_divideOverloading();
+    operator_remainderOverloading();
+    operator_assignmentsOverloading();
+    operator_RelationOverloading();
+    return 0;
+}
